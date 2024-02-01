ec942939dad0ca0bdae87132bed025e235d60398
Arpit Gupta <guptaarpit3323@gmail.com>
2024-02-02 00:55:50 +0530

    Reactor

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'arpitgupta' );
 define( 'DB_PASSWORD', 'Arpit#@!123' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost:10004' );
+define( 'DB_HOST', 'localhost' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
